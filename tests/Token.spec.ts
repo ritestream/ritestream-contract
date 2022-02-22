faa667573187eae6603536e97b888a8c7ab319cf
Alex Xiang <xiangzhe0812@gmail.com>
2022-02-23 09:47:22 +1000

    fix the issue from audit add more unit test

--- tests/Token.spec.ts
+++ tests/Token.spec.ts
@@ -6,10 +6,13 @@ import { getEventData, getRevertMessage } from "./utils";
 let token: tsEthers.Contract;
 let deployer: tsEthers.Signer;
 let user: tsEthers.Wallet;
+let otherUser: tsEthers.Signer;
+const vaultAddress = "0xa3A0Ce9592fE2bfA378Cc4dD5aB24Be150f00029"; //fake vault address for testing
 
 describe("ERC20 Token", () => {
   before(async () => {
     deployer = (await ethers.getSigners())[0];
+    otherUser = (await ethers.getSigners())[1];
     token = await (
       await ethers.getContractFactory("Token")
     ).deploy("Token", "TKN", 18);
@@ -28,32 +31,92 @@ describe("ERC20 Token", () => {
     expect(await token.decimals()).to.equal(18);
   });
 
-  it("Should burn tokens from deployer", async () => {
+  it("should be able to burn the tokens if the msg.sender is owner", async () => {
     const amount = ethers.BigNumber.from("10");
     const address = await deployer.getAddress();
-    await token.burn(address, amount);
+    await token.burn(amount);
     const balance = await token.balanceOf(address);
     expect(balance).to.equal(
       ethers.BigNumber.from("999999999999999999999999990")
     );
   });
 
-  it("Should only allow deployer to mint/burn", async () => {
-    // List protected functions.
-    let userToken = token.connect(user);
-    const ownerFunctions = [() => userToken.burn(user.address, "1")];
-    // Assert that all protected functions revert when called from an user.
-    for (let ownerFunction of ownerFunctions) {
-      try {
-        await expect(ownerFunction()).to.be.revertedWith(
-          "Ownable: caller is not the owner"
+  it("Should revert if the msg.sender is not owner while burning the tokens", async () => {
+    const amount = ethers.BigNumber.from("10");
+    await expect(token.connect(user).burn(amount)).to.be.revertedWith(
+      getRevertMessage("Ownable: caller is not the owner")
+    );
+  });
+
+  it("should approve tokens from holder to spender account through signature", async () => {
+    const userAddress = await user.getAddress();
+    const messageHash = await token
+      .connect(user)
+      .getMessageHash(userAddress, vaultAddress, "1000000000000000000000");
+    const signature = await user.signMessage(
+      ethers.utils.arrayify(messageHash)
+    );
+
+    await token.setAllowanceWithSignature(
+      userAddress,
+      vaultAddress,
+      "1000000000000000000000",
+      ethers.utils.arrayify(signature)
+    );
+
+    const userAfterAllowance = await token
+      .connect(user)
+      .allowance(userAddress, vaultAddress);
+
+    expect(userAfterAllowance).to.equal(
+      ethers.BigNumber.from("1000000000000000000000")
+    );
+  });
+
+  it("should revert on setting user’s allowance with signature if not called by owner", async () => {
+    const userAddress = await user.getAddress();
+    const messageHash = await token
+      .connect(user)
+      .getMessageHash(userAddress, vaultAddress, "1000000000000000000000");
+    const signature = await user.signMessage(
+      ethers.utils.arrayify(messageHash)
+    );
+
+    try {
+      await token
+        .connect(user)
+        .setAllowanceWithSignature(
+          userAddress,
+          vaultAddress,
+          "1000000000000000000000",
+          ethers.utils.arrayify(signature)
         );
-      } catch (error) {
-        // the solidity-coverage plugin is not smart enough to run the
-        // "revertedWith" unit test, so we account for that here.
-        if (!`${error}`.includes("sender doesn't have enough funds to send tx"))
-          throw error;
-      }
+    } catch (error) {
+      expect(getRevertMessage(error)).to.equal(
+        "Ownable: caller is not the owner"
+      );
+    }
+  });
+
+  it("should revert on use of other user's signature while approving through signature", async () => {
+    const userAddress = await user.getAddress();
+    const otherUserAddress = await otherUser.getAddress();
+    const messageHash = await token
+      .connect(user)
+      .getMessageHash(userAddress, vaultAddress, "1000000000000000000000");
+    const signature = await user.signMessage(
+      ethers.utils.arrayify(messageHash)
+    );
+
+    try {
+      await token.setAllowanceWithSignature(
+        otherUserAddress,
+        vaultAddress,
+        "1000000000000000000000",
+        ethers.utils.arrayify(signature)
+      );
+    } catch (error) {
+      expect(getRevertMessage(error)).to.equal("Not authorized");
     }
   });
 });
